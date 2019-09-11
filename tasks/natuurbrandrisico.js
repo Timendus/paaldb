88e05582de75b79c2363165341f305fc45626099
Tim Franssen <tim.franssen@nedap.com>
2019-09-11 10:57:58 +0200

    Fire hazard: Find the right regio for the right location

--- tasks/natuurbrandrisico.js
+++ tasks/natuurbrandrisico.js
@@ -1,20 +1,27 @@
-const Request = require('../util/request');
+const Request        = require('../util/request');
+const pointInPolygon = require('robust-point-in-polygon');
+const {Location}     = require('../models');
 
-module.exports.run = () => {
-  return new Request(`https://www.natuurbrandrisico.nl/maps/script_all_regios.js?ts=${Date.now().valueOf()}`)
-  .then(result => {
+module.exports.run = async () => {
+  let result = await new Request(`https://www.natuurbrandrisico.nl/maps/script_all_regios.js?ts=${Date.now().valueOf()}`);
+  const locations = await Location.findAll();
 
-    // Cut off the parts that aren't JSON
-    result = result.substring("NBR.RegionsDefinition = ".length);
-    result = result.substring(0, result.length - 1);
+  // Cut off the parts that aren't JSON
+  result = result.substring("NBR.RegionsDefinition = ".length);
+  result = result.substring(0, result.length - 1);
 
-    // Parse the rest
-    result = JSON.parse(result);
+  // Parse the rest
+  result = JSON.parse(result);
 
-    // Output for fun and giggles
-    result.forEach(r => {
-      console.log(r.name);
-    });
+  // Find the right region for each location
+  for ( const l of locations ) {
+    for ( const r of result ) {
 
-  });
+      // Is our location in this region?
+      if ( pointInPolygon(r.coords[0], [l.longitude, l.latitude]) < 1 ) {
+        console.log(`[${r.fase}] Location ${l.name} is in brandweer regio ${r.name}`);
+      }
+
+    }
+  }
 }
