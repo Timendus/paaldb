7fac08b65703a9886c9074b0ef9036ceed2ae44d
Tim Franssen <tim.franssen@nedap.com>
2019-09-15 21:33:15 +0200

    Add importers for scandinavian sources

---
+++ tasks/importers/udinaturen.js
@@ -0,0 +1,78 @@
+const Request      = require('../../util/request');
+const importHelper = require('../../util/import-helper');
+const root         = "https://admin.udinaturen.dk";
+
+module.exports.run = async () => {
+  // The Shelter app seems to query the 'ud i naturen' API for these categories:
+  //   38: Lille lejrplads (small camping)
+  //   39: Stor lejrplads (large camping)
+  //   40: Frit teltningsområde (free tenting area)
+  const locations = await fetchLocationsInCategories([38,39,40]);
+
+  // Collect source information
+  const source = {
+    name:        "Ud i naturen",
+    description: "'Ud i naturen' is een kaart en een database van het Deense Natuuragentschap Naturstyrelsen. Het Deense Natuuragentschap beheert ongeveer 200.000 hectare staatsbossen en natuurgebieden, om de grootst mogelijke waarde voor de samenleving te creëren in de vorm van een goed kader voor het buitenleven, bescherming van de natuur en efficiënt beheer van de bossen en andere natuurgebieden van het agentschap. Het bestuur beheert bijvoorbeeld Dyrehaven, het meest bezochte natuurgebied van Denemarken, grote gebieden in de Waddenzee, aangewezen als UNESCO Werelderfgoed, en Hanstholm Wildlife Reserve, het grootste duingebied van Denemarken.",
+    contact:     "<a href='https://naturstyrelsen.dk/udinaturen/om-udinaturen/'>Over 'Ud i naturen' (Deens)</a>"
+  };
+
+  // Collect the locations we mention
+  const mentions = locations.map(l => {
+    const coordinates = getCoordinates(l);
+    const attributes = (l.attributes.length > 0 ? l.attributes : l.subcategory.attributes).map(a => a.attribute);
+
+    return {
+      name:        l.name,
+      latitude:    coordinates[1],
+      longitude:   coordinates[0],
+      description: l.longdescription,
+
+      properties:  {
+        accessibleWithWheelchair: attributes.includes(1),
+        hasDrinkingWater:         attributes.includes(6),
+        hasShelter:               attributes.includes(9),
+        hasFireplace:             attributes.includes(10),
+        accessFromWater:          attributes.includes(11),
+        hasShower:                attributes.includes(12),
+        dogsOnLeashAllowed:       attributes.includes(13),
+        horsesAllowed:            attributes.includes(14),
+        accessibleWithStroller:   attributes.includes(15),
+        hasPrimitiveToilet:       attributes.includes(9001),
+        hasToilet:                attributes.includes(9002)
+      }
+    };
+  });
+
+  // Save this data
+  await importHelper.save({
+    task:       __filename,
+    projection: 'EPSG:25832',
+    source,
+    mentions
+  });
+}
+
+async function fetchLocationsInCategories(categories) {
+  let result;
+  let locations = [];
+  let next      = `/api/v1/facilityread/?format=json&subcategory__in=${categories.join(',')}&limit=500`;
+
+  // 'Ud i naturen' paginates its results
+  // Fetch all pages untill we have all locations
+  do {
+    result = await new Request(`${root}${next}`);
+    result = JSON.parse(result);
+    locations = locations.concat(result.objects);
+    next = result.meta.next;
+  } while ( next );
+
+  return locations;
+}
+
+function getCoordinates(location) {
+  const point = [location.the_geom, location.the_geom2]
+    .filter(g => g.type == 'Point')
+    .shift();
+
+  return point.coordinates || [null, null];
+}
---
+++ tasks/importers/vindskyddskartan.js
@@ -0,0 +1,51 @@
+const Request      = require('../../util/request');
+const importHelper = require('../../util/import-helper');
+const parser       = require('xml-js');
+const {Mention}    = require('../../models');
+
+// Function to query the map
+module.exports.run = async () => {
+  let result = await new Request("https://www.google.com/maps/d/kml?forcekml=1&mid=1doLXXvokOtiMzthVz0SLFjEPmD4");
+
+  // Parse KML file
+  result = parser.xml2js(result, {
+    compact:           true,
+    ignoreDeclaration: true,
+    ignoreAttributes:  true,
+    trim:              true
+  });
+
+  // Collect source information
+  const source = {
+    name:        "Vindskyddskartan Sverige",
+    description: "Zweedse kaart van alle shelters in Zweden. In Zweden mag immers overal wild gekampeerd worden, maar niet overal is een shelter met een vuurplaats. Communitykaart van een Facebookgroep.",
+    contact:     "<a href='https://www.facebook.com/groups/vindskyddinorden/'>De Facebookgroep van Vindskyddskartan</a>"
+  };
+
+  let placemarks = result.kml.Document.Folder.map(f => f.Placemark);
+  placemarks = [].concat.apply([], placemarks); // Flatten
+
+  // Collect the locations we mention
+  const mentions = placemarks.map(p => {
+    const [lon, lat] = p.Point.coordinates._text.split(',');
+
+    return {
+      name:        p.name._text,
+      latitude:    lat,
+      longitude:   lon,
+      description: p.description ? (p.description._cdata || p.description._text) : '',
+
+      properties: {
+        hasShelter:    p.styleUrl._text == '#icon-1197',
+        isUnconfirmed: p.styleUrl._text == '#icon-503-DB4436-nodesc'
+      }
+    }
+  });
+
+  // Save this data
+  await importHelper.save({
+    task: __filename,
+    source,
+    mentions
+  });
+}
