77e961f02e2a9e83b9f1c2162240a1c545503e61
Tim Franssen <tim.franssen@nedap.com>
2019-09-16 21:43:45 +0200

    Add interface to view locations

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use(express.static('public'));
 app.use(express.json());
 app.use('/api', api);
 app.use('/export', exporters);
---
+++ public/scripts.js
@@ -0,0 +1,102 @@
+window.addEventListener('load', async () => {
+  const mymap = L.map('map')
+                 .setView([52.232947, 5.697784], 8);
+
+  const tileLayer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}@2x?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'satellite-v9',
+    accessToken: 'pk.eyJ1IjoidGltZW5kdXMiLCJhIjoiY2swbWhueTlrMTNuZDNnc3ZqcWV6aG5kdyJ9.1uDVkJBRa0DdbU8PD_inog',
+    tileSize: 512,
+    zoomOffset: -1,
+    detectRetina: true
+  });
+  tileLayer.addTo(mymap);
+
+  // Change map type from satellite only to satellite plus streets when we zoom in
+  mymap.on('zoom', (event) => {
+    if ( event.target._zoom > 10 ) {
+      tileLayer.options.id = 'satellite-streets-v11';
+    } else {
+      tileLayer.options.id = 'satellite-v9';
+    }
+  });
+
+  // Hide location popup when we click the map
+  mymap.on('click', () => {
+    document.getElementById('location').classList.remove('active');
+  });
+
+  // Show location popup when we click a location pin
+  const locations = JSON.parse(await request('/api/locations'));
+  for ( const location of locations ) {
+    L.marker([location.latitude, location.longitude], {
+      title:   location.name,
+      alt:     location.name,
+      opacity: (Math.min(3, location.numberOfMentions)/3)
+    })
+    .addTo(mymap)
+    .on('click', () => {
+      showDetails(location);
+    });
+  }
+});
+
+function request(url) {
+  return new Promise((resolve, reject) => {
+    const httpRequest = new XMLHttpRequest()
+    httpRequest.addEventListener('readystatechange', (event) => {
+      if ( httpRequest.readyState !== XMLHttpRequest.DONE ) return;
+      if ( httpRequest.status !== 200 ) return reject("Received an error from the back-end");
+      resolve(httpRequest.responseText);
+    });
+
+    httpRequest.open('GET', url);
+    httpRequest.send();
+  });
+}
+
+async function showDetails(location) {
+  const details = JSON.parse(await request(`/api/locations/${location.id}`));
+
+  document.getElementById('location').innerHTML = `
+    <h1>${location.name}</h1>
+
+    ${ location.description ? `
+      <section class='description'>
+        ${location.description}
+      </section>
+    ` : '' }
+
+    ${ location.fireHazard  ? `
+      <section class='fireHazard'>
+        <p>Brandrisico: <b>${location.fireHazard}</b></p>
+      </section>
+    ` : '' }
+
+    <section class='mentions'>
+      <p>Deze locatie wordt vermeld door:</p>
+      <ul>
+        ${
+          details.Mentions.map(m => {
+            return `
+              <li>
+                <details>
+                  <summary><b>${m.Source.name}</b> - ${m.status}</summary>
+                  <h2>${m.name}</h2>
+                  ${ m.description ? `
+                    <section class='description'>
+                      ${m.description}
+                    </section>
+                  ` : '' }
+                </details>
+              </li>
+            `;
+          }).join('')
+        }
+      </ul>
+    </section>
+  `;
+
+  document.getElementById('location').classList.add('active');
+}
