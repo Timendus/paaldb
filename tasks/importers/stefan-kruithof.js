646017a3417fa4704b456f565994e8649da3c70e
Tim Franssen <tim.franssen@nedap.com>
2019-10-06 22:20:30 +0200

    Add links and descriptions where we can from Stefan's website

--- tasks/importers/stefan-kruithof.js
+++ tasks/importers/stefan-kruithof.js
@@ -1,32 +1,43 @@
-const Request      = require('../../util/request');
-const importHelper = require('../../util/import-helper');
-const convert      = require('xml-js');
-const {Mention}    = require('../../models');
+const Request         = require('../../util/request');
+const Logger          = require('../../util/logger');
+const importHelper    = require('../../util/import-helper');
+const roundCoordinate = require('../../util/round-coordinate');
+const {Mention}       = require('../../models');
+const parser          = require('node-html-parser');
+const convert         = require('xml-js');
 
 // Function to query the map
 module.exports.run = async () => {
-  let result = await new Request("https://www.google.com/maps/d/kml?forcekml=1&mid=1SdsMhcQBTrVjABZaLXYPJZeOgfA");
+
+  // Stefan's map is more up to date
+  let map = await new Request("https://www.google.com/maps/d/kml?forcekml=1&mid=1SdsMhcQBTrVjABZaLXYPJZeOgfA");
+  // But his website has more information
+  let website = await new Request("https://sites.google.com/site/paalkampeerders/system/app/pages/subPages?path=/locatiebeschrijvingen");
 
   // Parse KML file
-  result = convert.xml2js(result, {
+  map = convert.xml2js(map, {
     compact:           true,
     ignoreDeclaration: true,
     ignoreAttributes:  true,
     trim:              true
   });
 
+  // Parse HTML file
+  website = parser.parse(website);
+
   // Collect source information
   const source = {
     name:        "Stefan Kruithof",
-    description: result.kml.Document.description._cdata,
+    description: map.kml.Document.description._cdata,
     contact:     "<a href='mailto:stefankruithof@gmail.com'>stefankruithof@gmail.com</a>"
   };
 
-  // Collect the locations we mention
-  const mentions = result.kml.Document.Folder.Placemark.map((p) => {
+  // Collect the locations we mention from the map
+  const mentions = [];
+  for ( const p of map.kml.Document.Folder.Placemark ) {
     const [lon, lat] = p.Point.coordinates._text.split(',');
 
-    return {
+    mentions.push({
       name:        p.name._text,
       latitude:    lat,
       longitude:   lon,
@@ -36,8 +47,38 @@ module.exports.run = async () => {
       properties: {
         hasShelter: p.styleUrl._text == '#icon-117'
       }
+    });
+  }
+
+  // Add links and descriptions to mentions from the website
+  for ( const anchor of website.querySelectorAll('.sites-table a') ) {
+    try {
+      const link = `https://sites.google.com${anchor.attributes.href}`;
+      const page = parser.parse(await new Request(link));
+
+      // Take the page content, remove the messy top table
+      const content     = page.querySelector('.sites-tile-name-content-1 div').innerHTML;
+      const table       = page.querySelector('.sites-tile-name-content-1 div div').outerHTML;
+      const description = content.substr(table.length);
+
+      // Find something that matches coordinates anywhere in the page :/
+      const coordinates = content.match(/\d\d\.\d{3,8},\s+\d\.\d{3,8}/)[0];
+      const [lat, lon]  = coordinates.split(', ');
+
+      // Find the matching mention by rounded coordinates
+      const mention = mentions.find(m =>
+        roundCoordinate(m.latitude,  3) == roundCoordinate(lat, 3) &&
+        roundCoordinate(m.longitude, 3) == roundCoordinate(lon, 3)
+      );
+      if ( !mention ) continue;
+
+      // Add these properties to the mention
+      mention.link        = link;
+      mention.description = description;
+    } catch (error) {
+      Logger.error(error);
     }
-  });
+  }
 
   // Save this data
   await importHelper.save({
