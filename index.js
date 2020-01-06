b94f44f5189096f6d12f99ba4cfb7bacd0071221
Tim Franssen <tim.franssen@nedap.com>
2020-01-06 19:32:10 +0100

    Add CORS headers so we can use the API from the browser

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use('/api', (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 app.use(express.json());
 app.get('/status', status);
